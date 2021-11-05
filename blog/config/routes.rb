58882c1bb895f77b2359720658379e4a2d060ce0
Balazs <balazs.birta@gmail.com>
2021-11-05 19:09:36 +0100

    Created a basic CRUD Rails blog

---
+++ blog/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root "articles#index"                     #this defines the default landing page
+
+  get "/articles", to: "articles#index"     #"/articles" is the name of the controller, "articles#index #index is a method defined on the articles controller"
+  get "/articles/:id", to: "articles#show"  #"/articles/:id" :id is a route parameter, this captures a segment of the request's path and puts that in the params hash. Accessible by params[:id] in the show action of ArticlesController
+end
