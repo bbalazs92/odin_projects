27e469a2bb30bbb81f2ca24b1f34836a3aee07ee
Balazs <balazs.birta@gmail.com>
2021-11-08 16:45:52 +0100

    Added commenting funcionality to blog

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   #get "/articles", to: "articles#index"     #"/articles" is the name of the controller, "articles#index #index is a method defined on the articles controller"
   #get "/articles/:id", to: "articles#show"  #"/articles/:id" :id is a route parameter, this captures a segment of the request's path and puts that in the params hash. Accessible by params[:id] in the show action of ArticlesController
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
