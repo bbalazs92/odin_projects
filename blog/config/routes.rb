076c4f6b4f8fd0ee8731730f710448ece522e5ca
Balazs <balazs.birta@gmail.com>
2021-11-08 15:59:46 +0100

    Added posting funcionality to blog

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"                     #this defines the default landing page
 
-  get "/articles", to: "articles#index"     #"/articles" is the name of the controller, "articles#index #index is a method defined on the articles controller"
-  get "/articles/:id", to: "articles#show"  #"/articles/:id" :id is a route parameter, this captures a segment of the request's path and puts that in the params hash. Accessible by params[:id] in the show action of ArticlesController
+  #get "/articles", to: "articles#index"     #"/articles" is the name of the controller, "articles#index #index is a method defined on the articles controller"
+  #get "/articles/:id", to: "articles#show"  #"/articles/:id" :id is a route parameter, this captures a segment of the request's path and puts that in the params hash. Accessible by params[:id] in the show action of ArticlesController
+  resources :articles
 end
